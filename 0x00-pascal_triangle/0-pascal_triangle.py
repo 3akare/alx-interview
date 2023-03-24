32c49afb8f175b570a9fbabd8ad22c74be6a6ece
David Bakare <bakaredavid007@gmail.com>
2023-03-24 03:14:38 +0100

    removed imports, create factorial function with for loop

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,7 +2,18 @@
 ''' Techincal Interview: Pascal's Triangle'''
 
 
-# from math import factorial
+def factorial(n):
+
+    '''
+        factorial - return the factorial of a number
+        n: integer
+        return: integer
+    '''
+    factor = 1
+    while n != 0:
+        factor *= n
+        n -= 1
+    return factor
 
 
 def pascal_triangle(n):
@@ -13,20 +24,13 @@ def pascal_triangle(n):
         n: integer
         return: a list of list
     '''
-
-    try:
-        if n <= 0:
-            return []
-        iArray, oArray = [], []
-        for i in range(n):
-            for j in range(i + 1):
-                # nCr of each row and store in a list ie. n!/(n-r)!r!.
-                # In the case i!//(i - j)!*j!
-                # value = factorial(i)//(factorial(j)*factorial(i-j))
-                iArray.append(value)
-            oArray.append(iArray)
-            iArray = []
-        return oArray
-    except Exception:
-        pass
-print(pascal_triangle(5))
+    if n <= 0:
+        return []
+    iArray, oArray = [], []
+    for i in range(n):
+        for j in range(i + 1):
+            value = factorial(i)//(factorial(j)*factorial(i-j))
+            iArray.append(value)
+        oArray.append(iArray)
+        iArray = []
+    return oArray
