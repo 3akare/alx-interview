f4e54dfd74f15d45815a90a7064ea201921f49ce
David Bakare <bakaredavid007@gmail.com>
2023-04-02 21:33:08 +0100

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,22 +9,6 @@ def canUnlockAll(boxes):
     canUnlockAll function
     '''
 
-    len_of_box = len(boxes)
-    keys = set(boxes[0])
-    empty = False
-    empty_status = 0
-
-    for box in boxes:
-        if box == [] or len(box) == 0:
-            empty = True
-            empty_status += 1
-        for i in box:
-            keys.add(i)
-
-    for i in range(100):
-        if (empty or empty_status != 0):
-            if (i not in keys):
-                keys.add(i)
-                empty_status -= 1
-                break
-    return len(keys) == len_of_box
+    if (not isinstance(boxes, list) or len(boxes) == 0):
+        return False
+    return (True)
