7ef3210a059c92ed8d3303d8e1bb5b768edc882b
David Bakare <bakaredavid007@gmail.com>
2023-04-02 20:30:04 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+canUnlockAll function module
+'''
+
+
+def canUnlockAll(boxes):
+    '''
+    canUnlockAll function
+    '''
+
+    len_of_box = len(boxes)
+    keys = set(boxes[0])
+
+    for box in boxes:
+        if box == []:
+            keys.add(99)
+        for i in box:
+            keys.add(i)
+    return len(keys) == len_of_box
