fcc4c2fdc553bd1ddc74c0c1ef36c3a5c0ce1511
David Bakare <bakaredavid007@gmail.com>
2023-04-08 01:12:07 +0100

    minor updates

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,10 +12,12 @@ def minOperations(n):
     if n <= 1:
         return 0
     factors = []
-    for i in range(2, n):
+    for i in range(2, int(n**0.5) + 1):
         while n % i == 0:
             factors.append(i)
             n //= i
     if n > 1:
         factors.append(n)
+    if not factors:
+        return 0
     return sum(factors)
