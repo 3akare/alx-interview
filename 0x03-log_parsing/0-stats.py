79278a3f0a78d169d551afd3c33ce121b64ff8f4
David Bakare <bakaredavid007@gmail.com>
2023-04-14 20:47:55 +0100

    code refactor

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-    Reads from standard input and computes metrics.
-"""
 
+if __name__ == '__main__':
+    import sys
 
-def print_log(s_code, t_size, o_put):
-    """
-    print statictics
-        Args: s_code(dictionary)
-        - t_size(int)
-        - o_put(string)
-    Return: No return Value
-    """
-    print(o_put.format(t_size))
-    for k, v in s_code.items():
-        if v != 0:
-            print(f'{k}: {v}')
-
-
-def logrun():
-    """
-    Starts the log server
-    """
     o_put = '''File size: {}'''
-    t_size, num = 0, 0
+    t_size = 0
+    num = 0
     s_code = {
         '200': 0,
         '301': 0,
@@ -34,28 +22,35 @@ def logrun():
         '405': 0,
         '500': 0
         }
-
     try:
-        while True:
-            ln = input()
-            if not ln:
-                break
+        for ln in sys.stdin:
             if (num == 10):
-                print_log(s_code, t_size, o_put)
+                print(o_put.format(t_size))
+                for k, v in s_code.items():
+                    if v != 0:
+                        print(f'{k}: {v}')
                 num = 1
             else:
                 num += 1
+
+            ln = [ln.split()]
             try:
-                ln = [ln.split()]
                 t_size += int(ln[0][-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
                 s_code[ln[0][-2]] += 1
             except Exception:
                 pass
+        print(o_put.format(t_size))
+        for k, v in s_code.items():
+            if v != 0:
+                print(f'{k}: {v}')
 
-        print_log(s_code, t_size, o_put)
     except KeyboardInterrupt:
-        print_log(s_code, t_size, o_put)
-
-
-if __name__ == '__main__':
-    logrun()
+        print(o_put.format(t_size))
+        for k, v in s_code.items():
+            if v != 0:
+                print(f'{k}: {v}')
+        raise
