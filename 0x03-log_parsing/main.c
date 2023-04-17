a0079709e8f2a260ad684e7cf34e232f6cd466a9
David Bakare <bakaredavid007@gmail.com>
2023-04-17 10:53:42 +0100

    updated README, added utf-8 validate

--- 0x03-log_parsing/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-    int x, y, z;
-    scanf("%d %*d %d", &x, &y, &z);
-    printf("%d %d %d", x, y, z);
-}
---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+validUTF8 function module
+'''
+
+import re
+
+
+def validUTF8(data):
+    '''
+    validUTF8 function
+    '''
+
+    regex = [
+            r'[\x00-\x7F]',
+            r'[\xC2-\xDF][\x80-\xBF]',
+            r'[\xE0][\xA0-\xBF][\x80-\xBF]',
+            r'[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}',
+            r'\xED[\x80-\x9F][\x80-\xBF]',
+            r'\xF0[\x90-\xBF][\x80-\xBF]{2}',
+            r'\xF1-\xF3][\x80-\xBF]{3}',
+            r'\xF4[\x80-\x8F][\x80-\xBF]{2}'
+        ]
+    for d in data:
+        byte = chr(d).encode('utf-8').decode('utf-8')
+        for r in regex:
+            regex_obj = re.compile(r)
+            if (regex_obj.match(byte)):
+                break
+            else:
+                return False
+    return True
