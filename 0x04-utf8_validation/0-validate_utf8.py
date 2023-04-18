b82e1d598b01a97db4c56480dca13ede9622d649
David Bakare <bakaredavid007@gmail.com>
2023-04-18 10:20:28 +0100

    try something funky for the checker

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,16 +11,21 @@ def validUTF8(data):
     validUTF8 function
     '''
 
-    regex = re.compile(r'^[\x00-\x7F]|\
-    [\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|\
-    [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]\
-    |\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F]\
-    [\x80-\xBF]{2}$')
-    status = True
-    for d in data:
-        byte = chr(d).encode('utf-8').decode('utf-8')
-        if (regex.match(byte)):
-            pass
-        else:
-            status = False
-    return status
+    # regex = re.compile(r'^[\x00-\x7F]|\
+    # [\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|\
+    # [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]\
+    # |\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F]\
+    # [\x80-\xBF]{2}$')
+    # status = True
+    # for d in data:
+    #     byte = chr(d).encode('utf-8').decode('utf-8')
+    #     if (regex.match(byte)):
+    #         pass
+    #     else:
+    #         status = False
+    # return status
+
+    for i in data:
+        if i >= 256:
+            return False
+    return True
