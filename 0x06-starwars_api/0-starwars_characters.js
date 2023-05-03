36eb61efefe325644be88b8952a2504ff700d3c0
David Bakare <bakaredavid007@gmail.com>
2023-05-03 20:02:06 +0100

    semi standard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,32 @@
 const request = require('request');
 
 const getCharacter = async () => {
-    const MovieId = process.argv[2];
-    const url = `https://swapi-api.alx-tools.com/api/films/${MovieId}`;
-    request(url, async (err, res, body) => {
-      if (err) console.log(err);
-      else {
-        const data = JSON.parse(body);
-        try{
-            for (const element of data.characters) {
-                (
-                  await new Promise((resolve, reject) => {
-                    request(element, (err, res, body) => {
-                      if (err) reject(err);
-                      else {
-                        JSON.parse(body).name ? console.log(JSON.parse(body).name) : console.log(err);
-                        resolve();
-                      }
-                    });
-                  }
-                  )
-                );
-              }
+  const MovieId = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${MovieId}`;
+  request(url, async (err, res, body) => {
+    if (err) console.log(err);
+    else {
+      const data = JSON.parse(body);
+      try {
+        for (const element of data.characters) {
+          (
+            await new Promise((resolve, reject) => {
+              request(element, (err, res, body) => {
+                if (err) reject(err);
+                else {
+                  JSON.parse(body).name ? console.log(JSON.parse(body).name) : console.log(err);
+                  resolve();
+                }
+              });
+            }
+            )
+          );
         }
-        catch(err){
-          console.log(res.statusCode);
-        }
+      } catch (err) {
+        console.log(res.statusCode);
       }
-    });
+    }
+  });
 };
 
 getCharacter();
