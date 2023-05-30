446713f5912f640a53f186ac715d22afd6d989de
David Bakare <bakaredavid007@gmail.com>
2023-05-30 17:49:17 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 '''
-Prime Game: Technical Interview
-'''    
+isWinner() Function
+'''
+
+
 def isWinner(x, nums):
-    number = 0  # or grundy number  
-    for ak in range(x):
-        number ^= nums[ak % len(nums)];
+    '''
+    isWinner. Declares the winner in a strange game
+    '''
+    if x < 1 or not nums:
+        return None
 
-    return "Ben" if number > 0 else "Maria"
+    maria_wins = 0
+    ben_wins = 0
 
+    for n in nums:
+        primes_count = sum(1 for i in range(2, n + 1) if all(i % j != 0 for j in range(2, int(i ** 0.5) + 1)))  # noqa
+        if primes_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins == ben_wins:
+        return None
+    elif maria_wins > ben_wins:
+        return "Maria"
+    else:
+        return "Ben"
